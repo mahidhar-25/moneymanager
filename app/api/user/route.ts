95d9703b711b542437eb45532882af366c3234ed
mahidhar reddy <mahidhar100008@gmail.com>
2024-07-22 15:59:15 +0530

    updated singleton prisma client and server actions

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from "@prisma/client";
-const client = new PrismaClient();
+import client from "@/db/index";
 export async function POST(req: NextRequest) {
     const body = await req.json();
 
